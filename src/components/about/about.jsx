97bc1e70e33785a2da18e99f3cd598250bd0eab2
Your Name <you@example.com>
2025-06-24 17:18:41 +0530

    connect to about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,6 +2,7 @@ import Counter from "@/common/counter";
 import HeaderTwo from "@/layout/header/header-two";
 import React from "react";
 import AboutArea from "./about-area";
+import AboutFirst from "./about-first";
 import Banner from "./banner";
 import Specialists from "@/common/specialists";
 import NavTab from "@/common/nav-tab";
@@ -9,12 +10,13 @@ import Team from "@/common/team";
 import FooterFour from "@/layout/footer/footer-4";
 import ContactUs from "../contact/contact";
 import ContactForm from "../contact/contact-form";
-import AboutFirst from "./about-first";
+
 
 const About = () => {
   return (
     <>
       <HeaderTwo />
+      <Aboutfirst />
       <Banner />
       <AboutArea />
       <Counter />
@@ -22,7 +24,6 @@ const About = () => {
       <NavTab />
       <Team />
       <FooterFour />
-      <AboutFirst />
     </>
   );
 };
