e7bec987e72014d2260dec4d35f04698b17f740e
Your Name <you@example.com>
2025-06-24 10:28:08 +0530

    link of blog

--- src/components/about/banner.jsx
+++ src/components/about/banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
             <div className="col-xl-5 col-lg-12 col-md-12 col-12">
               <div className="tp-breadcrumb__link text-xl-end">
                 <span>
-                  Dimer Scientific : <Link href="/team-details"> Blog</Link>
+                  Dimer Scientific : <Link href="/blog"> Blog</Link>
                 </span>
               </div>
             </div>
