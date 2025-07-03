c2e87051eeff999ad53887b095509c7b431b7bf4
Your Name <you@example.com>
2025-07-03 13:52:36 +0530

    Changed image and styling in breadcrumbs

--- src/components/contact/banner.jsx
+++ src/components/contact/banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
       <section
         className="breadcrumb__area pt-100 pb-120 breadcrumb__overlay"
         style={{
-          backgroundImage: `url("/assets/img/banner/breadcrumb-01.jpg")`,
+          backgroundImage: `url("/assets/img/banner/breadcrumb-background.png")`,
         }}
       >
         <div className="container">
