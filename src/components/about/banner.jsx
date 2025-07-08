d28137b353f6072ed05693ccf47d01e3ea9d1b40
Your Name <you@example.com>
2025-07-08 17:57:52 +0530

    error solve

--- src/components/about/banner.jsx
+++ src/components/about/banner.jsx
@@ -7,11 +7,7 @@ const Banner = () => {
       <section
         className="breadcrumb__area pt-100 pb-120 breadcrumb__overlay"
         style={{
-<<<<<<< HEAD
-          backgroundImage: `url(/assets/img/banner/breadcrumb-01.jpg)`,  
-=======
           backgroundImage: `url(/assets/img/banner/breadcrumb-background.png)`,
->>>>>>> 5e0b3eb200eacc315ddc54cd47bd5ab09b0e6d31
         }}
       >
         <div className="container">
@@ -24,16 +20,8 @@ const Banner = () => {
             <div className="col-xl-5 col-lg-12 col-md-12 col-12">
               <div className="tp-breadcrumb__link text-xl-end">
                 <span>
-<<<<<<< HEAD
-                  Dimer Scientific : 
-                      <div className="breadcrumb">
-                          <a href="/" className="breadcrumb-link">Home</a>
-                                  <span className="breadcrumb-separator">›</span>
-                                  <span className="breadcrumb-current">About Us</span>
-                      </div>
-=======
+
                   <a href="/">Home</a>/<li className="breadcrumb-item active" aria-current="page">About us</li>
->>>>>>> 5e0b3eb200eacc315ddc54cd47bd5ab09b0e6d31
                 </span>
               </div>
             </div>
