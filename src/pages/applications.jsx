8210d1e3816e65b2172249616a71c16e83b845d7
Kishan <kishan@oceantechnolab.com>
2025-07-21 17:00:39 +0530

    application page is done

--- src/pages/applications.jsx
+++ src/pages/applications.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Table from "@/components/periodic-table/table";
 import Wrapper from "@/layout/wrapper";
 import SEO from "@/components/seo";
-import Periodic from "@/components/periodic-table/periodic";
-import Applications from "@/components/application/applications";
+import Application from "@/components/application/application";
 
 const index = () => {
   return (
     <Wrapper>
-        <Applications />
+        <Application />
       <SEO pageTitle="Periodic Table" />
     </Wrapper>
   );
