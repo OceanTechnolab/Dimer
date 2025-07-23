aaaf108fa068e768cc4c71d88aa3fbdcc6c4b8a5
Kishan <kishan@oceantechnolab.com>
2025-07-23 10:07:29 +0530

    banner tect change

--- src/components/Certificates/banner.jsx
+++ src/components/Certificates/banner.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Banner = () => {
@@ -13,21 +14,15 @@ const Banner = () => {
           <div className="row align-items-center">
             <div className="col-xl-7 col-lg-12 col-md-12 col-12">
               <div className="tp-breadcrumb">
-                <h2 className="tp-breadcrumb__title">Certificates </h2>
+                <h2 className="tp-breadcrumb__title">Certificates</h2>
               </div>
             </div>
             <div className="col-xl-5 col-lg-12 col-md-12 col-12">
               <div className="tp-breadcrumb__link text-xl-end">
-                <nav aria-label="breadcrumb">
-                  <ol className="breadcrumb">
-                    <li className="breadcrumb-item">
-                      <a href="/">Home</a>
-                    </li>
-                    <li className="breadcrumb-item active" aria-current="page">
-                      Certificates
-                    </li>
-                  </ol>
-                </nav>
+                <span>
+
+                  <a href="/">Home</a>/<li className="breadcrumb-item active" aria-current="page">Certificates</li>
+                </span>
               </div>
             </div>
           </div>
